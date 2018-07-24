3c3483fd5846f57544be7e9a99f25083e34149ac
Michał Kupiecki <michal481216@gmail.com>
2018-07-25 00:07:00 +0200

    add button up

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,9 @@ window.onload = function() {
 				$('.logo-container').css('min-width', '7rem');
 				$('nav').css('height', '1.2rem');
 				$('.nav-elem').css('height', '1.2rem');
+				//btn up
+				
+				$('#btnCircleUp-js').css('display', 'block');
 			} else {
 				//reverse change
 				console.log('reverse change');
@@ -22,7 +25,16 @@ window.onload = function() {
 				$('.logo-container').removeAttr('style');
 				$('nav').removeAttr('style');
 				$('.nav-elem').removeAttr('style');
+				//btn up
+				$('#btnCircleUp-js').removeAttr('style');
 			}
 		});
+	});
+	// button UP
+	jQuery(function scrollUp() {
+		$('#btnCircleUp-js').on( "click", function(event) {
+			event.preventDefault();
+			$('html, body').animate({scrollTop: 0}, 800);
+		  });
 	});
 };
