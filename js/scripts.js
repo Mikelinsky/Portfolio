488cd253279c3d40ed07db8ce0b1bbf80f883c30
Michał Kupiecki <michal481216@gmail.com>
2018-07-26 14:18:51 +0200

    fixed navbar

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,33 @@
 window.onload = function() {
 	//navbar
-	jQuery(function changeNavbar() {
+	jQuery(function watchScroll() {
 		$(window).scroll(function() {
 			var positionOfScroll = $(window).scrollTop();
-			
 			console.log(positionOfScroll);
 			
-			if (positionOfScroll >= 500) {
+			if (positionOfScroll >= 450) {
 				//change navbar
 				console.log('change navbar');
-				$('.small-logo').addClass('visible');
+				$('.navbar-container').css('top', '5.5rem');
 				$('.big-logo').addClass('hidden');
+				$('.small-logo').addClass('visible');
 				$('.logo-container').css('min-width', '7rem');
 				$('nav').css('height', '1.2rem');
 				$('.nav-elem').css('height', '1.2rem');
-				//btn up
-				
+				$('li').css('line-height', '1rem');
+				//change btn up
 				$('#btnCircleUp-js').css('display', 'block');
 			} else {
-				//reverse change
+				//reverse change navbar
 				console.log('reverse change');
-				$('.big-logo').removeClass('hidden');
+				$('.navbar-container').removeAttr('style');
 				$('.small-logo').removeClass('visible');
+				$('.big-logo').removeClass('hidden');
 				$('.logo-container').removeAttr('style');
 				$('nav').removeAttr('style');
 				$('.nav-elem').removeAttr('style');
-				//btn up
+				$('li').removeAttr('style'); 
+				//reverse change btn up
 				$('#btnCircleUp-js').removeAttr('style');
 			}
 		});
