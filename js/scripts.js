e8b44556fbc059c4256edf189203d9ca1be89edc
Michał Kupiecki <michal481216@gmail.com>
2018-08-16 14:50:20 +0200

    add margin bottom to authorphoto

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,7 @@ window.onload = function() {
 					$('.logo-container').css('min-width', '7rem');
 					$('nav').css('height', '1.2rem');
 					$('.nav-elem').css('height', '1.2rem');
-					$('.hamburger-li').css('line-height', '1rem');
+					$('.navbar-li').css('line-height', '1rem');
 				};
 				changeNavbar();
 				//change btn up
@@ -34,7 +34,7 @@ window.onload = function() {
 					$('.logo-container').removeAttr('style');
 					$('nav').removeAttr('style');
 					$('.nav-elem').removeAttr('style');
-					$('.hamburger-li').removeAttr('style'); 
+					$('.navbar-li').removeAttr('style'); 
 				};
 				reverseChangeNavbar();
 				//reverse change btn up
@@ -49,19 +49,26 @@ window.onload = function() {
 			$('html, body').animate({scrollTop: 0}, 800);
 		  });
 	});
-	//hamburger
+	//hamburger menu
 	jQuery(function showMenu() {
+		/* open menu */
 		$('.hamburger').click(function() {
 			$('.hamburger-menu').show(700);
 			$('.hamburger').hide();
 			$('.hamburger-close').show();
 			$('.responsive-navbar').css('background-color', 'rgba(16, 31, 85, 0.49)');
 		});
+		/* close menu */
 		$('.hamburger-close').click(function() {
 			$('.hamburger-menu').hide(700);
 			$('.hamburger').show();
 			$('.hamburger-close').hide();
 			$('.responsive-navbar').removeAttr('style');
 		});
+	});
+	jQuery(function selectAndClose() {
+		$('.hamburger-menu-link').click(function() {
+			
+		})
 	});
 };
